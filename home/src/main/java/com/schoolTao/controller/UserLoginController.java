5f03931ed312d0bf57acd897fb6b84bb15b4e3c1
Yfreshman <asus@freshman-pc>
2019-11-19 14:32:52 +0800

    userlogin

---
+++ home/src/main/java/com/schoolTao/controller/UserLoginController.java
@@ -0,0 +1,17 @@
+package com.schoolTao.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mysql.jdbc.log.Log;
+import com.mysql.jdbc.log.LogFactory;
+
+@RestController
+@RequestMapping("/login")
+@Validated
+public class UserLoginController {
+	 //  private final Log logger = LogFactory.getLog(UserLoginController .class);
+	
+
+}
